9946fbabee5d8e8dfcb521c59a60c34e84b519a1
Oluwaseun Taiwo <taiwoluwaseun8@gmail.com>
2023-01-26 09:25:03 +0100

    modified git ignore

--- dist/middlewares/error-handler.js
+++ dist/middlewares/error-handler.js
@@ -4,11 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const zod_1 = require("zod");
-const client_1 = require("@prisma/client");
+// import { Prisma } from "@prisma/client";
 const settings_1 = __importDefault(require("../config/settings"));
 const custom_errors_1 = require("../utils/custom-errors");
 const errorHandler = (error, req, res, next) => {
-    var _a;
     console.log(error);
     if (error instanceof custom_errors_1.CustomHttpError) {
         return res.status(error.statusCode).json({ message: error.message });
@@ -19,18 +18,18 @@ const errorHandler = (error, req, res, next) => {
         });
         return res.status(422).json({ errors });
     }
-    if (error instanceof client_1.Prisma.PrismaClientKnownRequestError) {
-        if (error.code === "P2002") {
-            const fields = (_a = error.meta) === null || _a === void 0 ? void 0 : _a.target;
-            const messages = fields.map((field) => {
-                const message = field.endsWith("Id")
-                    ? "User cannot have more than one record"
-                    : `${field} already exists`;
-                return message;
-            });
-            return res.status(400).json({ message: messages.join(", ") });
-        }
-    }
+    // if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    //   if (error.code === "P2002") {
+    //     const fields: Array<string> = error.meta?.target as Array<string>;
+    //     const messages: Array<string> = fields.map((field) => {
+    //       const message = field.endsWith("Id")
+    //         ? "User cannot have more than one record"
+    //         : `${field} already exists`;
+    //       return message;
+    //     });
+    //     return res.status(400).json({ message: messages.join(", ") });
+    //   }
+    // }
     // server errors will return the error in development
     // but "Internal Server Error" will be sent in production
     if (settings_1.default.NODE_ENV === "development") {
