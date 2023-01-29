02f8b6b2c70df05848b1308f9d194169943eb6de
Oluwaseun Taiwo <taiwoluwaseun8@gmail.com>
2023-01-29 11:32:10 +0100

    get video price implementation done

--- src/controllers/video.controller.ts
+++ src/controllers/video.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import prisma from "../services/prisma";
 import { CreateVideoInputType } from "../schemas/video.schema";
+import { NotFound } from "../utils/custom-errors";
 
 export const createVideo = async (
   req: Request<{}, {}, CreateVideoInputType["body"]>,
@@ -44,4 +45,20 @@ export const getVideos = async (req: Request, res: Response) => {
   const videos = await prisma.video.findMany();
 
   return res.status(200).json({ videos });
+};
+
+export const getVideoPrice = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const video = await prisma.video.findUnique({ where: { id } });
+  if (!video) {
+    throw new NotFound("Video not found");
+  }
+
+  const price = await prisma.price.findUnique({ where: { videoId: id } });
+  if (!price) {
+    throw new NotFound("Price not found");
+  }
+
+  return res.status(201).json({ price });
 };
--- src/routes/video.route.ts
+++ src/routes/video.route.ts
@@ -1,11 +1,16 @@
 import express from "express";
-import { createVideo, getVideos } from "../controllers/video.controller";
+import {
+  createVideo,
+  getVideoPrice,
+  getVideos,
+} from "../controllers/video.controller";
 import { validateInput } from "../middlewares/input-validator";
 import { CreateVideoInputSchema } from "../schemas/video.schema";
 
 const router = express.Router();
 
 router.get("/", getVideos);
+router.get("/:id/price", getVideoPrice);
 
 router.post("/upload", validateInput(CreateVideoInputSchema), createVideo);
 
